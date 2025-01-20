2d3d9734e8d2c8d657da74d4847550828d9127f2
shin.jewoo <shinjw0926@naver.com>
2025-01-20 23:27:51 +0900

    first commit

---
+++ src/main/java/com/minipay/MiniPayApplication.java
@@ -0,0 +1,13 @@
+package com.minipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minipay/MiniPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
