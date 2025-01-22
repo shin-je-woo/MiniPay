942481ef9a36f98a8391ff85779d737076a668ee
shin.jewoo <shinjw0926@naver.com>
2025-01-22 23:34:50 +0900

    feat: Membership 도메인 모델링

---
+++ membership/src/main/java/com/minipay/membership/domain/Membership.java
@@ -0,0 +1,52 @@
+package com.minipay.membership.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Membership {
+    private final String membershipId;
+    private final String name;
+    private final String email;
+    private final String address;
+    private final boolean isValid;
+    private final boolean isCorp;
+
+    public static Membership generateMember(
+            MembershipId membershipId,
+            MembershipName membershipName,
+            MembershipEmail membershipEmail,
+            MembershipAddress membershipAddress,
+            MembershipIsValid membershipIsValid,
+            MembershipIsCorp membershipIsCorp
+    ) {
+        return new Membership(
+                membershipId.id,
+                membershipName.name,
+                membershipEmail.email,
+                membershipAddress.address,
+                membershipIsValid.isValid,
+                membershipIsCorp.isCorp
+        );
+    }
+
+    public record MembershipId(String id) {
+    }
+
+    public record MembershipName(String name) {
+    }
+
+    public record MembershipEmail(String email) {
+    }
+
+    public record MembershipAddress(String address) {
+    }
+
+    public record MembershipIsValid(boolean isValid) {
+    }
+
+    public record MembershipIsCorp(boolean isCorp) {
+    }
+}
