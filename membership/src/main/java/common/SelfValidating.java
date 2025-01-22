895a00b23771153f9e9892d4519475b3c3bbe8d0
shin.jewoo <shinjw0926@naver.com>
2025-01-22 23:35:51 +0900

    feat: Hexagonal 관련 마커 애노테이션, 객체 검증을 위한 SelfValidating

---
+++ membership/src/main/java/common/SelfValidating.java
@@ -0,0 +1,23 @@
+package common;
+
+import jakarta.validation.*;
+
+import java.util.Set;
+
+public abstract class SelfValidating<T> {
+
+    private static final ValidatorFactory FACTORY = Validation.buildDefaultValidatorFactory();
+    private static final Validator VALIDATOR = FACTORY.getValidator();
+
+    protected SelfValidating() {
+    }
+
+    protected void validateSelf() {
+        @SuppressWarnings("unchecked")
+        T self = (T) this;
+        Set<ConstraintViolation<T>> violations = VALIDATOR.validate(self);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+}
