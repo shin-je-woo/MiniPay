3fcb7e99e21fcbb2f4eafd1b1e1a9c1f3415c406
shin.jewoo <shinjw0926@naver.com>
2025-01-25 23:37:24 +0900

    test: Membership 등록

---
+++ membership/src/test/java/com/minipay/membership/adapter/in/web/RegisterMembershipControllerTest.java
@@ -0,0 +1,51 @@
+package com.minipay.membership.adapter.in.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.minipay.membership.domain.Membership;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RegisterMembershipControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void registerMembership() throws Exception {
+        RegisterMembershipRequest request = new RegisterMembershipRequest("name", "email", "address", false);
+
+        Membership expectedMembership = Membership.generateMember(
+                new Membership.MembershipId("1"),
+                new Membership.MembershipName(request.name()),
+                new Membership.MembershipEmail(request.email()),
+                new Membership.MembershipAddress(request.address()),
+                new Membership.MembershipIsValid(true),
+                new Membership.MembershipIsCorp(request.isCorp())
+        );
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/membership")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(
+                        MockMvcResultHandlers.print()
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().string(objectMapper.writeValueAsString(expectedMembership))
+                );
+    }
+}
