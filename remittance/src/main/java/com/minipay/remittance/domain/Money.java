10390caab48e5d2e988a14fe9f579e27bce7c1b3
shin.jewoo <shinjw0926@naver.com>
2025-03-07 06:39:56 +0900

    feat: Remittance 도메인 모델링

---
+++ remittance/src/main/java/com/minipay/remittance/domain/Money.java
@@ -0,0 +1,28 @@
+package com.minipay.remittance.domain;
+
+import com.minipay.common.exception.DomainRuleException;
+
+import java.math.BigDecimal;
+
+public record Money(BigDecimal value) {
+
+    public Money {
+        if (value == null || value.compareTo(BigDecimal.ZERO) < 0) {
+            throw new DomainRuleException("money is less than zero");
+        }
+    }
+
+    public static Money ZERO = new Money(BigDecimal.ZERO);
+
+    public Money add(Money money) {
+        return new Money(this.value.add(money.value));
+    }
+
+    public Money subtract(Money money) {
+        return new Money(this.value.subtract(money.value));
+    }
+
+    public boolean isNegative() {
+        return this.value.compareTo(BigDecimal.ZERO) < 0;
+    }
+}
---
+++ remittance/src/main/java/com/minipay/remittance/domain/Remittance.java
@@ -0,0 +1,79 @@
+package com.minipay.remittance.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Remittance {
+
+    private final RemittanceId remittanceId;
+    private final Sender sender;
+    private final Recipient recipient;
+    private final RemittanceType remittanceType;
+    private final RemittanceStatus remittanceStatus;
+    private final Money amount;
+
+    // Factory
+    public static Remittance newInstance(
+            Sender sender,
+            Recipient recipient,
+            Money money
+    ) {
+        return new Remittance(RemittanceId.generate(), sender, recipient, recipient.getRemittanceType(), RemittanceStatus.REQUESTED, money);
+    }
+
+    // VO
+    public record RemittanceId(UUID value) {
+        public RemittanceId {
+            if (value == null) {
+                throw new IllegalArgumentException("RemittanceId cannot be null");
+            }
+        }
+
+        private static RemittanceId generate() {
+            return new RemittanceId(UUID.randomUUID());
+        }
+    }
+
+    public record Sender(
+            UUID membershipId
+    ) {
+        public Sender {
+            if (membershipId == null) {
+                throw new IllegalArgumentException("Sender cannot be null");
+            }
+        }
+    }
+
+    public record Recipient(
+            UUID membershipId,
+            String bankName,
+            String accountNumber
+    ) {
+        public Recipient {
+            // membershipId가 있으면 내부고객, 없으면 외부은행
+            if (membershipId == null && (bankName == null || accountNumber == null)) {
+                throw new IllegalArgumentException("Invalid Recipient");
+            }
+        }
+
+        public RemittanceType getRemittanceType() {
+            return membershipId == null ? RemittanceType.EXTERNAL : RemittanceType.INTERNAL;
+        }
+    }
+
+    public enum RemittanceType {
+        INTERNAL,
+        EXTERNAL
+    }
+
+    public enum RemittanceStatus {
+        REQUESTED,
+        SUCCEEDED,
+        FAILED
+    }
+}
