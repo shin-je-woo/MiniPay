541376a66352ce4b68dd849fc756615f8fe26dd4
shin.jewoo <shinjw0926@naver.com>
2025-04-23 22:49:23 +0900

    refactor: ApiAdapter의 응답값 requireNonNull 제거

--- minipay-banking/src/main/java/com/minipay/banking/adapter/out/service/MembershipServiceApiAdapter.java
+++ minipay-banking/src/main/java/com/minipay/banking/adapter/out/service/MembershipServiceApiAdapter.java
@@ -4,7 +4,6 @@ import com.minipay.banking.application.port.out.MembershipServicePort;
 import com.minipay.common.annotation.MiniPayServiceAdapter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Objects;
 import java.util.UUID;
 
 @MiniPayServiceAdapter
@@ -15,7 +14,7 @@ public class MembershipServiceApiAdapter implements MembershipServicePort {
 
     @Override
     public boolean isValidMembership(UUID membershipId) {
-        MembershipResponse membershipResponse = Objects.requireNonNull(membershipFeignClient.getMembership(membershipId).getBody());
+        MembershipResponse membershipResponse = membershipFeignClient.getMembership(membershipId).getBody();
         return membershipResponse.isValid();
     }
 }
