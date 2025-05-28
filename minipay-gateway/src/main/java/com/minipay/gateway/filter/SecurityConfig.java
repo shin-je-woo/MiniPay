c612bdee2a1938d2bbe6df1f7220b5e06508d923
shin.jewoo <shinjw0926@naver.com>
2025-05-28 21:45:21 +0900

    feat: gateway 기본 security 비활성화

---
+++ minipay-gateway/src/main/java/com/minipay/gateway/filter/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.minipay.gateway.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .headers(headers -> headers.frameOptions(frameOptions ->
+                        frameOptions.mode(XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN)))
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .build();
+    }
+}
